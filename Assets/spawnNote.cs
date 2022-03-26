35a24872fb6e9670ebfd8f012d74f7c7919f592a
Richard_R <37909374+nomodfx@users.noreply.github.com>
2022-03-26 16:56:56 -0700

    Add files via upload

--- Assets/spawnNote.cs
+++ Assets/spawnNote.cs
@@ -4,35 +4,70 @@ using UnityEngine;
 
 public class spawnNote : MonoBehaviour
 {
-    /*public Vector3 startPosition;
+    public Vector3 startPosition;
     public Vector3 endPosition;
-    public float secondsOfLife;*/
+    //public float secondsOfLife;
     public GameObject noteObject;
+    //public GameObject[] noteObject = new GameObject[4];
+    //public float metronome;
+    //private float targetLocation;
+    public float spawnRate = 1.0f;
+    public int count;
 
-    void Start() 
+    private IEnumerator Start() 
     {
-        //Match speed with metronome
-        InvokeRepeating("SpawnNote", 1.00f, 1.00f); //methodname, time in seconds, repeatRate seconds
-            //SpawnNote();
-        
+        yield return StartCoroutine(spawn());
+        Destroy(gameObject, 0.1f);
     }
-    
-    // Update is called once per frame
-    void Update()
+
+    private IEnumerator spawn()
     {
-        /*if (Input.GetKeyDown(KeyCode.Space))
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+
+            Instantiate(noteObject, transform.position, noteObject.transform.rotation);
+            noteObject.transform.position += Vector3.right;
+
+      
+            yield return new WaitForSeconds(0.50f);
+        }
+
+        /*for (int i = 0; i < noteObject.Length; i++)
+        {
+            while (noteObject[i].transform.position.x < targetLocation)
+            {
+                noteObject[i].transform.Translate(moveSpeed * Time.deltaTime * Vector3.right);
+                yield return null;
+
+                GameObject noteItem = Instantiate(noteObject[i], transform.position, transform.rotation);
+                noteItem.transform.parent = gameObject.transform;
+                noteItem.name = gameObject.name + "-note";
+                //Destroy(noteObject[i], 0.5f);
+                yield return new WaitForSeconds(0.55f); //stop infinite spawning
+            }
+            Destroy(noteObject[i]);
+        }*/
+    }
+    // Update is called once per frame
+    /*void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
 
             SpawnNote();
         
-        }*/
+        }
     }
 
-    void SpawnNote()
+    /*void SpawnNote()
     {
-      
-        GameObject noteItem = Instantiate(noteObject, transform.position, transform.rotation);
-        noteItem.transform.parent = gameObject.transform;
-        noteItem.name = gameObject.name + "-note";
-    }
+
+        Instantiate(noteObject, transform.position, noteObject.transform.rotation);
+        Destroy(gameObject, 1.3189f);
+        noteObject.transform.position += Vector3.right;
+        
+
+    }*/
+
 }
